fdbacccb211e3ede3792437b4c88cb6365100f01
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-18 22:35:29 +0530

    Refacored : Ability to add new contact.

--- com/bridgelabz/AddressBookTool/AddressBookSystem.java
+++ com/bridgelabz/AddressBookTool/AddressBookSystem.java
@@ -7,42 +7,63 @@ public class AddressBookSystem {
 
     Scanner scanner = new Scanner(System.in);
 	
-    /* Ability to create contact by taking input
+    /* Ability to create & add contact by taking input
      * from user */
     public void addContact() {
         Contact contact = new Contact();
-        System.out.println("Enter a first name:");
+        System.out.println("Enter first name :");
         String firstName = scanner.next();
         contact.setFirstName(firstName);
         
-        System.out.println("Enter a last name:");
+        System.out.println("Enter last name :");
         String lastName = scanner.next();
         contact.setLastName(lastName);
         
-        System.out.println("Enter a Address:");
+        System.out.println("Enter address :");
         String address = scanner.next();
         contact.setAddress(address);
         
-        System.out.println("Enter a City name:");
+        System.out.println("Enter city name:");
         String city = scanner.next();
         contact.setCity(city);
         
-        System.out.println("Enter a state:");
+        System.out.println("Enter state :");
         String state = scanner.next();
         contact.setState(state);
         
-        System.out.println("Enter a email:");
+        System.out.println("Enter email :");
         String email = scanner.next();
         contact.setEmail(email);
         
-        System.out.println("Enter a zip code:");
+        System.out.println("Enter pin code :");
         int zip = scanner.nextInt();
         contact.setZip(zip);
         
-        System.out.println("Enter a phone number:");
+        System.out.println("Enter mobile number :");
         int mobileNumber = scanner.nextInt();
         contact.setMobileNumber(mobileNumber);
         
-        System.out.println("Contact added successfully");
+        System.out.println("Contact added successfully.");
+    }
+    
+    //display menu for contact operations
+    void showMenu() {
+    	boolean exit = false;
+    	while(!exit) {
+    		System.out.println("1. Add Contact.\n2. Exit.");
+    		int choice = scanner.nextInt();
+    		switch(choice) {
+    			case 1:
+    				addContact();
+    				break;
+    			case 2:
+    				exit = true;
+    				System.out.println("Thanks,exiting menu.");
+    				break;
+    			default:
+    				System.out.println("Invalid Choice.");
+    		}
+    	}
     }
 }	
+
--- com/bridgelabz/AddressBookTool/AddressBookSystemMain.java
+++ com/bridgelabz/AddressBookTool/AddressBookSystemMain.java
@@ -4,7 +4,11 @@ public class AddressBookSystemMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome To Address Book System.");
+		System.out.println("Welcome To Address Book System.\n");
+		
+		AddressBookSystem addressBook = new AddressBookSystem();
+		//using addressBook object to call display menu method
+		addressBook.showMenu();
 
 	}
 
