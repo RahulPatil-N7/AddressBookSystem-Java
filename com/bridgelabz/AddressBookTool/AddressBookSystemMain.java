e66c99ff95b9e79d8ffa5d2ff041a776230ae1ef
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-18 21:53:13 +0530

    Added : welcome message.

---
+++ com/bridgelabz/AddressBookTool/AddressBookSystemMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.AddressBookTool;
+
+public class AddressBookSystemMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Address Book System.");
+
+	}
+
+}
